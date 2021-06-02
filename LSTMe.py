0723bffb8b7e552321db3625fcded2269c9853db
jerryGithub <jerry_jzy@126.com>
2021-06-02 15:42:20 +0800

    优化函数，实现负荷预测

--- LSTMe.py
+++ LSTMe.py
@@ -10,7 +10,7 @@ from sklearn.preprocessing import MinMaxScaler
 
 
 # 对数据进行处理
-def create_dataset(dataset, look_back):
+def createTrainDataset(dataset, look_back):
 #这里的look_back与timestep相同
     dataX, dataY = [], []
     for i in range(len(dataset)-look_back-1):
@@ -18,6 +18,29 @@ def create_dataset(dataset, look_back):
         dataX.append(a)
         dataY.append(dataset[i + look_back])
     return numpy.array(dataX),numpy.array(dataY)
+
+
+def readDataset(csvDath):
+    dataframe = pd.read_csv(csvDath, usecols=[1])  # 'airline-passengers.csv'
+    # train_seq = readDataCsv(csvpath, 1)
+    dataset = dataframe.values
+    # dataset = train_seq
+    # 将整型变为float
+    dataset = dataset.astype('float32')
+    # 归一化 在下一步会讲解
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    dataset = scaler.fit_transform(dataset)
+    return dataset
+
+
+# 对数据进行处理
+def createPredictDataset(dataset, look_back):
+#这里的look_back与timestep相同
+    dataX= []
+    for i in range(len(dataset)-look_back-1):
+        a = dataset[i:(i+look_back)]
+        dataX.append(a)
+    return numpy.array(dataX)
 
 
 def reshapeData(csvDath):
@@ -38,8 +61,8 @@ def reshapeData(csvDath):
 
     #训练数据太少 look_back并不能过大
     look_back = 96
-    trainX,trainY  = create_dataset(trainlist,look_back)
-    testX,testY = create_dataset(testlist,look_back)
+    trainX,trainY  = createTrainDataset(trainlist,look_back)
+    testX,testY = createTrainDataset(testlist,look_back)
 
     trainX = numpy.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
     testX = numpy.reshape(testX, (testX.shape[0], testX.shape[1] ,1 ))
@@ -51,20 +74,14 @@ def reshapeData(csvDath):
 # create and fit the LSTM network
 # LSTM的输入为 [samples, timesteps, features]
 # 这里的timesteps为步数，features为维度 这里我们的数据是1维的
-def LSTMfit(trainX, trainY):
+def LSTMfit(trainX, trainY, saveName):
     model = Sequential()
     model.add(LSTM(4, input_shape=(None,1)))
     model.add(Dense(1))
     model.compile(loss='mean_squared_error', optimizer='adam')
     model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
-    model.save(os.path.join("DATA","JS01Test" + ".h5"))
+    model.save(os.path.join("DATA", saveName + ".h5"))
 
-# make predictions
-def makePredic(trainX, testX):
-    model = load_model(os.path.join("DATA","JS01Test" + ".h5"))
-    trainPredict = model.predict(trainX)
-    testPredict = model.predict(testX)
-    return trainPredict, testPredict
 
 #反归一化
 def normalizationData(scaler, trainY, trainPredict, testY, testPredict):
@@ -75,7 +92,7 @@ def normalizationData(scaler, trainY, trainPredict, testY, testPredict):
     return trainY, trainPredict, testY, testPredict
 
 
-def pltData(trainY, trainPredict, testY, testPredict):
+def plt2Data(trainY, trainPredict, testY, testPredict):
     plt.figure()
 
     plt.subplot(211)
@@ -87,13 +104,36 @@ def pltData(trainY, trainPredict, testY, testPredict):
     plt.plot(testPredict[1:])
     plt.show()
 
+
+def pltData(dataY, dataPredict):
+    plt.figure()
+    plt.plot(dataY, color="blue")
+    plt.plot(dataPredict[1:], color="red")
+    plt.legend(('original', 'predict'), loc='upper right')
+    plt.xticks(range(0, len(dataY) ,96))
+    plt.show()
+
+
+def dataTrainMain(csvData, saveName):
+    scaler, trainX, testX, trainY, testY = reshapeData(csvData)
+    LSTMfit(trainX, trainY, saveName)
+
+
+def dataPredictMain(modelName, csvData):
+    model = load_model(os.path.join("DATA", modelName + ".h5"))
+    dataset = readDataset(csvData)
+    dataX, dataY = createTrainDataset(dataset, 96)
+    dataPredict = model.predict(dataX)
+    return dataPredict, dataY
+
+
 if __name__ == '__main__':
     # csvData = 'airline-passengers.csv'
-    csvData = 'F:\Work\科研项目\\2020.3\河北\江苏曲线\\01\cuv.csv'
-    scaler, trainX, testX, trainY, testY = reshapeData(csvData)
+    csvData = r'F:\Work\科研项目\\2020.3\河北\江苏曲线\\cuv.csv'
+    # saveName = r'LSTM_JS01_10_96'
+    saveName = r'LSTM_JS01_96'
+    # dataTrainMain(csvData, saveName)
 
-    #LSTMfit(trainX, trainY)
-
-    trainPredict, testPredict = makePredic(trainX, testX)
-    trainY, trainPredict, testY, testPredict = normalizationData(scaler, trainY, trainPredict, testY, testPredict)
-    pltData(trainY, trainPredict, testY, testPredict)
+    csvPredictData = r'F:\Work\科研项目\\2020.3\河北\江苏曲线\\03\cuv.csv'
+    dataPredict, dataY = dataPredictMain(saveName, csvPredictData)
+    pltData(dataY, dataPredict)
